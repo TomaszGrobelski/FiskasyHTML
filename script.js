da5e8f952aae17717c10acec68276f1a09a9c528
Tomasz <bleniog@interia.pl>
2024-09-13 23:19:45 +0200

    Fiskasy fixes some response elements

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ document.querySelectorAll('.menu-toggle').forEach((button) => {
     }
   });
 });
+
+//Language switch UA/GB/PL
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.language-button');
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', function () {
+      buttons.forEach((btn) => btn.classList.remove('active'));
+
+      this.classList.add('active');
+    });
+  });
+});
